e7df5cd115484fc60d80e6f21808e83d90c5d815
Ravi RT Merugu <rrmerugu@gmail.com>
2017-06-05 02:50:53 +0530

    import fixes

--- trawler/trawler.py
+++ trawler/trawler.py
@@ -1,4 +1,4 @@
-from .browsers import BrowseBing, BrowseStackOverFlow
+from trawler.browsers import BrowseBing, BrowseStackOverFlow
 import selenium.webdriver as webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
@@ -7,25 +7,26 @@ class TrawlIt(object):
     """
     This will run a browser search and returns the data with some extra operations on top
     of BrowseBing class
-    
+
     USAGE:
         from trawler import TrawlIt
-        
+
         trawl = TrawlIt(kw="MongoDB", generate_kws=True)
         trawl.generated_keywords # ['learning MongoDB', 'Programming with MongoDB', 'MongoDB tutorials' ]
         trawl.run() # this will gather data from all generated keywords and searches all
-        
+        trawl.stop()
+
         # or
-        
+
         trawl = TrawlIt(kw="MongoDB")
         trawl.run() # this will gather data
-        
+        trawl.stop()
 
-    
+
     """
- 
-    _SUFFIXES = [ 'tutorials', ]
-    _PREFIXES = [ 'learning', 'Programming with' ]
+    
+    _SUFFIXES = ['tutorials', ]
+    _PREFIXES = ['learning', 'Programming with']
     _AVAILABLE_BROWSERS = ['bing', 'stackoverflow']
     
     def __init__(self, kw=None, browser='bing', max_pages=3,
@@ -41,21 +42,21 @@ class TrawlIt(object):
         self._DATA = {
             'results': [],
             'results_count': 0,
-            'related_keywords' : [],
-            'related_keywords_count' : 0,
+            'related_keywords': [],
+            'related_keywords_count': 0,
             'search_kw': '',
             'search_kw_generated': []
-        
+            
         }
         if prefixes: self._PREFIXES = prefixes
         if suffixes: self._SUFFIXES = suffixes
         
         if self._BROWSER not in self._AVAILABLE_BROWSERS:
             raise NotImplementedError("Only [%s] search is implemented at this moment, "
-                                      "contact author for more info" %(",".join(self._AVAILABLE_BROWSERS)))
+                                      "contact author for more info" % (",".join(self._AVAILABLE_BROWSERS)))
         
         self._init_browser_instance()
-            
+    
     @property
     def generated_keywords(self):
         if len(self._GENERATED_KEYWORDS) == 0:
@@ -64,7 +65,7 @@ class TrawlIt(object):
             else:
                 self._GENERATED_KEYWORDS = [self._KEYWORD]
         return self._GENERATED_KEYWORDS
-        
+    
     @property
     def data(self):
         return self._DATA
@@ -72,16 +73,16 @@ class TrawlIt(object):
     def _init_browser_instance(self):
         self._DRIVER = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub',
                                         desired_capabilities=DesiredCapabilities.CHROME)
-        
+    
     def _generate_keywords(self):
         keywords = []
         for prefix in self._PREFIXES:
             keywords.append("%s %s" % (prefix, self._KEYWORD))
-    
+        
         for suffix in self._SUFFIXES:
             keywords.append("%s %s" % (self._KEYWORD, suffix))
         return keywords
-
+    
     def _append_data(self, data):
         self._DATA['results'] += data['results']
         self._DATA['related_keywords'] += data['related_keywords']
@@ -89,35 +90,35 @@ class TrawlIt(object):
         self._DATA['results_count'] += data['results_count']
         self._DATA['search_kw'] = self._KEYWORD
         self._DATA['search_kw_generated'] = self.generated_keywords
-        
+    
     def _run(self, kw):
         if self._BROWSER == 'bing':
             browser = BrowseBing(kw=kw, max_page=self._MAX_PAGES, driver=self._DRIVER)
         elif self._BROWSER == 'stackoverflow':
             browser = BrowseStackOverFlow(kw=kw, max_page=self._MAX_PAGES, driver=self._DRIVER)
-            
+        
         browser.search()
         print "Gathered the data for keyword", kw
         self._append_data(browser.data)
-        
+    
     def run(self):
         """
         Runs the data gathering jobs -
             **if self._GENERATE_KWS == True:** new keywords will be generated based on the prefixes, and suffixes is
             True, it will iterate through each keyword and gathers the information **else:** single keyword
             provided to the __init__() will be used and gathered data.
-            
+
         :return:
-        
+
         """
         self._GENERATED_KEYWORDS = self.generated_keywords
         if self._GENERATE_KWS:
-            print "Generated %s keywords for [%s] " %(len(self._GENERATED_KEYWORDS), self._KEYWORD)
+            print "Generated %s keywords for [%s] " % (len(self._GENERATED_KEYWORDS), self._KEYWORD)
             for kw in self._GENERATED_KEYWORDS:
                 self._NOW_KEYWORD = kw
                 self._run(self._NOW_KEYWORD)
         else:
             self._run(self._NOW_KEYWORD)
-            
+    
     def stop(self):
         self._DRIVER.close()
