219457c15a0f4dbba0bbde798e63c17974b5220a
Ravi RT Merugu <rrmerugu@gmail.com>
2017-06-04 18:46:48 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 
+dependencies = [p.rstrip('\n') for p in open('requirements/requirements.txt')]
+
 setup(
     name='trawler',
     version='1.0.0',
@@ -11,5 +13,6 @@ setup(
     license='',
     author='Ravi RT Merugu',
     author_email='rrmerugu@gmail.com',
-    description='A data gathering framework to search and get information from search engines like Bing'
+    description='A data gathering framework to search and get information from search engines like Bing',
+    install_requires=dependencies
 )
