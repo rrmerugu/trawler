940f6e1595c3c0c13c66c9d1d4348ee25ef0ade9
Ravi RT Merugu <rrmerugu@gmail.com>
2017-06-04 18:17:18 +0530

    setup fixes

--- setup.py
+++ setup.py
@@ -4,14 +4,12 @@ setup(
     name='trawler',
     version='1.0.0',
     packages=['trawler',
-              'trawler.db',
               'trawler.browsers',
-              'trawler.server',
               'tests'
               ],
     url='https://github.com/invaana/trawler',
     license='',
     author='Ravi RT Merugu',
     author_email='rrmerugu@gmail.com',
-    description='This is a data aggregation framework for scouting and aggregating Scientific Data. '
+    description='A data gathering framework to search and get information from search engines like Bing'
 )
