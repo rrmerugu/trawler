7d8a1eb78f51f6078ca7f1e3fad6833cca216ea5
Ravi RT Merugu <rrmerugu@gmail.com>
2017-06-05 01:45:14 +0530

    setup fix reverted

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ dependencies = [p.rstrip('\n') for p in open('requirements/requirements.txt')]
 setup(
     name='trawler',
     version='1.0.0',
-    packages=['trawler.trawler', ],
+    packages=['trawler', ],
     url='https://github.com/invaana/trawler',
     license='',
     author='Ravi RT Merugu',
