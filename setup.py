ee26a8cd00d3ccb26b92b13879de13619380e47d
Ravi RT Merugu <rrmerugu@gmail.com>
2018-01-10 13:48:23 +0530

    version skipped to 1.3 because this has breaking changes on the result data

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='trawler',
-    version='1.2.6',
+    version='1.3',
     packages=['trawler', 'tests' ],
     url='https://github.com/rrmerugu/trawler',
     license='',
