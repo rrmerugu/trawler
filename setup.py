618cdd081798e6e25e60859dfbd51c4f98d33426
Ravi RT Merugu <rrmerugu@gmail.com>
2018-09-05 05:25:49 +0530

    version updated to 1.3.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='trawler',
-    version='1.3.4',
+    version='1.3.5',
     packages=['trawler', 'trawler.browsers', 'tests' ],
     url='https://github.com/rrmerugu/trawler',
     license='',
