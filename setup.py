d0abc525ad6a55ae6d7541f5079978c3c1b6f9f9
Ravi Raja Merugu <rrmerugu@gmail.com>
2018-10-30 19:09:35 +0530

    version increased to 1.3.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='trawler',
-    version='1.3.5',
+    version='1.3.6',
     packages=['trawler', 'trawler.browsers', 'tests' ],
     url='https://github.com/rrmerugu/trawler',
     license='',
@@ -12,12 +12,12 @@ setup(
     author_email='rrmerugu@gmail.com',
     description='A data gathering framework to search and get information from web sources',
     install_requires=[
-        'cssselect==1.0.1',
-        'lxml==4.1.0',
-        'selenium==3.6.0',
-        'six==1.11.0',
-        'beautifulsoup4==4.6.0',
-        'requests==2.18.4',
-        'user-agent==0.1.9'
+        'cssselect',
+        'lxml',
+        'selenium',
+        'six',
+        'beautifulsoup4',
+        'requests',
+        'user-agent'
     ],
 )
